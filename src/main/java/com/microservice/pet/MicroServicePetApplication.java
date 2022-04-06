a980236aa6818cf1c3a35fe3f7f3c884f090d62d
cuadraO <omar.cuadrado@umgconsult.com>
2022-04-06 14:45:51 -0500

    [TICKET-00] - creacion del microservicio pet

---
+++ src/main/java/com/microservice/pet/MicroServicePetApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicePetApplication.class, args);
+	}
+
+}
